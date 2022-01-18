34e7331d6c5ee8aa4287ee9449de1eb657cd4849
Tobin <tobin.pomeroy@gmail.com>
2022-01-18 01:53:24 -0800

    first

---
+++ iOS-Pod/Class1.h
@@ -0,0 +1,18 @@
+//
+//  Class1.h
+//  iOS-Pod
+//
+//  Created by Tobin on 1/18/22.
+//
+
+#ifndef Class1_h
+#define Class1_h
+
+
+#endif /* Class1_h */
+
+@interface Class1 : NSObject
+
++(void)doStuff;
+
+@end
---
+++ iOS-Pod/iOS_Pod.h
@@ -0,0 +1,18 @@
+//
+//  iOS_Pod.h
+//  iOS-Pod
+//
+//  Created by Tobin on 1/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOS_Pod.
+FOUNDATION_EXPORT double iOS_PodVersionNumber;
+
+//! Project version string for iOS_Pod.
+FOUNDATION_EXPORT const unsigned char iOS_PodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_Pod/PublicHeader.h>
+
+#import "Class1.h"
